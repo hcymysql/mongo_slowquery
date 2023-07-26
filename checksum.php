69397d07c9fa9fd996f96130e186a45d456d7244
贺春旸 <chunyang_he@139.com>
2023-07-26 13:19:39 +0800

    适配MongoDB 5.0
    适配MongoDB 5.0

--- checksum.php
+++ checksum.php
@@ -2,7 +2,7 @@
 
 // 将JSON值替换为问号?
 function checksum($jsonData){
-
+//$jsonData ='{"find":"TaobaoDetailTransBasicInfoWrapper","filter":{"userId":"949836","channelCode":"qianzhan"},"projection":{"$sortKey":{"$meta":"sortKey"}},"sort":{"ct":-1},"limit":30,"shardVersion":[{"sec":1447,"inc":0},{"$id":"58a6c473b6e31bac07661a9f"}]}';  
 	$jsonArray = json_decode($jsonData,true); 	
 
 	foreach ($jsonArray as $key=>$value){  
@@ -11,8 +11,12 @@ function checksum($jsonData){
 			$jsonArray[$key]='?';
 		}
 	
-		foreach($value as $k=>$v){
-			$jsonArray[$key][$k]='?';
+		if (is_array($value)) {
+			foreach ($value as $k => $v) {
+				$jsonArray[$key][$k] = '?'; // 将嵌套数组中的值设置为 '?'
+			}
+		} else {
+			$jsonArray[$key] = '?'; // 将非数组的值设置为 '?'
 		}
 
 	}
@@ -23,3 +27,4 @@ function checksum($jsonData){
 }
 
 ?>
+
